b738c8fab48760ce80ff99250097feab461f527f
Nkwada Nora Solonko <nkwadanora@mail.com>
2024-01-08 20:17:28 -0800

    done with js

--- random.js
+++ random.js
@@ -1,17 +1,17 @@
 const container = document.querySelector('.container')
-const unslashURL = "https://source.unsplach.com/random/"
+const unsplashURL = 'https://source.unsplash.com/random/'
 const rows = 10
 
 for (let i = 0; i < rows * 3; i++) {
-    const img = document.createElement('img')
-    img.src = `${unsplashURL}${getRandomSize()}`
-    container.appendChild(img)
+  const img = document.createElement('img')
+  img.src = `${unsplashURL}${getRandomSize()}`
+  container.appendChild(img)
 }
 
-function getRandomSize() {
-    return `${getRandomUrl()}x${getRandomNr()}`
+function getRandomSize () {
+  return `${getRandomNr()}x${getRandomNr()}`
 }
 
-function getRandomNr() {
-    return Math.floor(Math.random() * 10) + 300
+function getRandomNr () {
+  return Math.floor(Math.random() * 10) + 300
 }
